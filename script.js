701098224c5f3581bc7c48d8e2d9cec69b835294
liaimv <liaimv0301@gmail.com>
2025-04-18 12:31:57 -0400

    Add quiz and pages

--- script.js
+++ script.js
@@ -20,3 +20,77 @@ function displayTime() {
 }
 
 setInterval(displayTime, 1000);
+
+//Quiz
+let quiz = []
+
+fetch('quiz.json')
+    .then(response => response.json())
+    .then(data => {
+        quiz = data;
+        startQuiz();
+    });
+  
+
+function startQuiz() {
+    let currentQuestion = 0;
+    let sleepTarget = 8;
+
+    const question = document.querySelector('.question');
+    const answers = document.querySelectorAll('.answers');
+    const buttons = document.querySelectorAll('.answers button');
+
+    function loadQuestion(index) {
+        const q = quiz[index];
+        question.textContent = q.question;
+
+        buttons.forEach((btn, i) => {
+            const answerData = q.answers[i]
+            btn.textContent = answerData.text;
+            btn.onclick = () => handleAnswer(answerData);
+        });
+    }
+
+    function handleAnswer(answerData) {
+        //Change sleep target time
+        sleepTarget += answerData.sleepEffect;
+        console.log(sleepTarget);
+
+        //Skip questions
+        if (answerData.text === "I didn't sleep last night" || answerData.text == "I don't have any assignment to do today" || answerData.text == "I don't have any tests tomorrow") {
+            currentQuestion += 2;
+        }
+        else {
+           currentQuestion++; 
+        }
+        
+        if (currentQuestion < quiz.length) {
+            loadQuestion(currentQuestion);
+        }
+        else {
+            if (sleepTarget > 10){
+                sleepTarget = 10;
+            }
+            question.textContent = `You should sleep ${sleepTarget} hours today!`;
+            answers.forEach(div => {
+                div.style.display = 'none';
+            });
+        }
+    }
+
+    loadQuestion(currentQuestion);
+}
+
+// Audio
+document.querySelectorAll('img[data-sound]').forEach(img => {
+    const soundId = img.getAttribute('data-sound');
+    const audio = document.getElementById(soundId);
+
+    img.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+});
